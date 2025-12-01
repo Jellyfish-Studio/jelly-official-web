48bebdd06712067b4592bdf6dbc09aea010af675
Jett Liu <haiheng000@gmail.com>
2025-12-02 00:28:15 +0800

    initial commit

---
+++ src/components/Home/Footer/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./index.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className={styles.content}>
+        © 2025 Jellyfish Studio. All Rights Reserved.
+      </div>
+    </footer>
+  );
+};
+
+export { Footer };
